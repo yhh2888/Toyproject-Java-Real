fa9a5322c19f9de7818eb61061b2b6f86c06b8ec
yhh2888 <yhh2888@naver.com>
2026-08-26 12:48:23 +0900

    uncomplete jsp2

--- src/main/java/com/office/toypjt/IToyPjtConfig.java
+++ src/main/java/com/office/toypjt/IToyPjtConfig.java
@@ -2,6 +2,8 @@ package com.office.toypjt;
 
 public interface IToyPjtConfig {
 
+	public final boolean IS_DEV = true;
+	
 	public final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	public final String URL = "jdbc:mysql://127.0.0.1:3306/db_member";
 	public final String USER = "root";
