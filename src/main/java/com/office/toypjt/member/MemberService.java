115be26ba2c33b048516b6a404a52c5e8385526d
kylego <kylego.studio@gmail.com>
2026-08-26 13:49:15 +0900

    MemoDto, MemberService.

--- src/main/java/com/office/toypjt/member/MemberService.java
+++ src/main/java/com/office/toypjt/member/MemberService.java
@@ -4,62 +4,87 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class MemberService {
-	
+
+    private String CLASS_NAME = "[MemberService]";
     private MemberDao memberDao;
 
     public MemberService(MemberDao memberDao) {
-		this.memberDao = memberDao;
-	}
-    
-    public int signUp(MemberDto memberDto) {
+        this.memberDao = memberDao;
+    }
+
+
+    private int signUp(MemberDto memberDto) {
         int result = memberDao.insertNewMember(memberDto);
 
         if (result > 0) {
-            System.out.println("[MemberService] signUp() SUCCESS!!");
+            System.out.println(CLASS_NAME.concat("signUp() SUCCESS!!"));
         } else {
-            System.out.println("[MemberService] signUp() FAIL!!");
+            System.out.println(CLASS_NAME.concat("signUp() FAIL!!"));
         }
 
         return result;
     }
 
-    public MemberDto signIn(MemberDto memberDto) {
-//        MemberDto loginedMember = memberDao.selectMember(memberDto);
-//
-//        if (loginedMember != null) {
-//            System.out.println("[MemberService] signIn() SUCCESS!!");
-//        } else {
-//            System.out.println("[MemberService] signIn() FAIL!!");
-//        }
-//
-//        return loginedMember;
-    	
-    	return null;
-    	
+    private MemberDto signIn(MemberDto memberDto) {
+        MemberDto loginedMember = memberDao.selectMember(memberDto);
+
+        if (loginedMember != null) {
+            System.out.println(CLASS_NAME.concat("signIn() SUCCESS!!"));
+        } else {
+            System.out.println(CLASS_NAME.concat("signIn() FAIL!!"));
+        }
+
+        return loginedMember;
     }
 
-	public int userSignUpMember(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	public String userSignInMember(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public int userSignUpMember(HttpServletRequest request, HttpServletResponse response) {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setId(request.getParameter("id"));
+        memberDto.setPw(request.getParameter("pw"));
 
-	public MemberDto getcurrentSigninedMember(String signinedMemberId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        return signUp(memberDto);
+    }
 
-	public int modifyMemberNo(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public String userSignInMember(HttpServletRequest request, HttpServletResponse response) {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setId(request.getParameter("id"));
+        memberDto.setPw(request.getParameter("pw"));
 
-	public int deleteMember(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+        MemberDto loginedMember = signIn(memberDto);
+
+        return (loginedMember != null) ? loginedMember.getId() : null;
+    }
+
+    public MemberDto getcurrentSigninedMember(String signinedMemberId) {
+        return memberDao.selectMemberById(signinedMemberId);
+    }
+
+    public int modifyMemberNo(HttpServletRequest request, HttpServletResponse response) {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setId(request.getParameter("id"));
+
+        int result = memberDao.updateMember(memberDto);
+
+        if (result > 0) {
+            System.out.println(CLASS_NAME.concat("modifyMemberNo() SUCCESS!!"));
+        } else {
+            System.out.println(CLASS_NAME.concat("modifyMemberNo() FAIL!!"));
+        }
+
+        return result;
+    }
+
+    public int deleteMember(HttpServletRequest request, HttpServletResponse response) {
+        String id = request.getParameter("id");
+        int result = memberDao.deleteMember(id);
+
+        if (result > 0) {
+            System.out.println(CLASS_NAME.concat("deleteMember() SUCCESS!!"));
+        } else {
+            System.out.println(CLASS_NAME.concat("deleteMember() FAIL!!"));
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/office/toypjt/memo/MemoDto.java
+++ src/main/java/com/office/toypjt/memo/MemoDto.java
@@ -1,24 +1,29 @@
 package com.office.toypjt.memo;
 
+import java.sql.Timestamp;
+
 public class MemoDto {
-    
-    private int memoNo;             // memo_no (메모 번호, PK)
-    private String memoName;        // memo_name (메모 제목)
-    private String memoContent;     // memo_content (메모 내용)
-    private String memoWriterId;    // memo_writer_id (작성자 아이디)
-    private String memoWriteDate;   // memo_write_date (작성 날짜)
+
+    private int memoNo;                // memo_no (메모 번호, PK)
+    private String memoName;           // memo_name (메모 제목)
+    private String memoContent;        // memo_content (메모 내용)
+    private String memoWriterId;       // memo_writer_id (작성자 아이디)
+    private Timestamp memoWriteDate;   // memo_write_date (작성 날짜)
+    private Timestamp memoLastModify;  // memo_last_modify (마지막 수정 날짜)
 
     // 기본 생성자
     public MemoDto() {
     }
 
     // 매개변수 생성자 (전체 필드)
-    public MemoDto(int memoNo, String memoName, String memoContent, String memoWriterId, String memoWriteDate) {
+    public MemoDto(int memoNo, String memoName, String memoContent, String memoWriterId,
+                   Timestamp memoWriteDate, Timestamp memoLastModify) {
         this.memoNo = memoNo;
         this.memoName = memoName;
         this.memoContent = memoContent;
         this.memoWriterId = memoWriterId;
         this.memoWriteDate = memoWriteDate;
+        this.memoLastModify = memoLastModify;
     }
 
     // Getter & Setter
@@ -54,18 +59,27 @@ public class MemoDto {
         this.memoWriterId = memoWriterId;
     }
 
-    public String getMemoWriteDate() {
+    public Timestamp getMemoWriteDate() {
         return memoWriteDate;
     }
 
-    public void setMemoWriteDate(String memoWriteDate) {
+    public void setMemoWriteDate(Timestamp memoWriteDate) {
         this.memoWriteDate = memoWriteDate;
+    }
+
+    public Timestamp getMemoLastModify() {
+        return memoLastModify;
+    }
+
+    public void setMemoLastModify(Timestamp memoLastModify) {
+        this.memoLastModify = memoLastModify;
     }
 
     // toString()
     @Override
     public String toString() {
         return "MemoDto [memoNo=" + memoNo + ", memoName=" + memoName + ", memoContent=" + memoContent
-                + ", memoWriterId=" + memoWriterId + ", memoWriteDate=" + memoWriteDate + "]";
+                + ", memoWriterId=" + memoWriterId + ", memoWriteDate=" + memoWriteDate
+                + ", memoLastModify=" + memoLastModify + "]";
     }
 }
